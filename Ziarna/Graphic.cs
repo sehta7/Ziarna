c86c2c380e325130550d5d41329cc929cf5fcc15
sehta7@gmail.com <sehta7@gmail.com>
2019-01-04 23:30:17 +0100

    add class to graphics

---
+++ Ziarna/Graphic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ziarna
+{
+    class Graphic
+    {
+        public Bitmap Bitmap { get; set; }
+        public Graphics Graphics { get; set; }
+
+        public Bitmap DrawGrains(List<Grain> grains)
+        {
+            Graphics = Graphics.FromImage(Bitmap);
+            foreach (var grain in grains)
+            {
+                Graphics.DrawRectangle(grain.PenColor, grain.PositionX, grain.PositionY, 1, 1);
+            }
+
+            return Bitmap;
+        }
+    }
+}
