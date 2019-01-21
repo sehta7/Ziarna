f5a4e4d97bb93bf7ab8bfd5a7c631716c56a2039
sehta7@gmail.com <sehta7@gmail.com>
2019-01-21 14:34:45 +0100

    initialize first tests

---
+++ GrainsTests/BoardTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ziarna;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ziarna.Tests
+{
+    [TestClass()]
+    public class BoardTests
+    {
+        [TestMethod()]
+        public void InitializeGrainTablesTest()
+        {
+            //Arrange
+            List<Grain> grains = new List<Grain>();
+            Grain[,] grainsInPreviousStep = new Grain[2, 2];
+            Grain[,] grainsInCurrentStep = new Grain[2, 2];
+            Board board = new Board(grains, grainsInPreviousStep, grainsInCurrentStep);
+
+            //Act
+            board.InitializeGrainTables(2, 2);
+
+            //Assert
+            foreach (var grain in board.GrainsInCurrentStep)
+            {
+                Assert.IsNotNull(grain);
+            }
+
+            foreach (var grain in board.GrainsInPreviousStep)
+            {
+                Assert.IsNotNull(grain);
+            }
+        }
+    }
+}
---
+++ GrainsTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GrainsTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GrainsTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ea1c5712-b8f4-4f30-ac77-2175229e45c3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
